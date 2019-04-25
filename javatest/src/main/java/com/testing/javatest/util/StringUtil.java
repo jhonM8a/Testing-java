a6f8f4d06ef3b836731529b540c125c9e3b1cfb7
jhonochoa <m8alemus@gmail.com>
2019-04-24 21:49:20 -0500

    Se añade funcion de validar si un string es vacio y sus test's

--- javatest/src/main/java/com/testing/javatest/util/StringUtil.java
+++ javatest/src/main/java/com/testing/javatest/util/StringUtil.java
@@ -10,16 +10,32 @@ package com.testing.javatest.util;
 public class StringUtil {
 
 	public static String repeat(String str, int times) {
-		
-		if(times<0){
+
+		if (times < 0) {
 			throw new IllegalArgumentException("times negative not allow");
 		}
 		String result = "";
-		
+
 		for (int i = 0; i < times; i++) {
 			result += str;
 		}
-		
+
 		return result;
+	}
+
+	public static boolean isEmpty(String str) {
+
+		if (str == null) {
+			return true;
+		}
+
+		if (str.equals("")) {
+			return true;
+		}
+
+		if(str.trim().isEmpty()) {
+			return true;
+		}
+		return false;
 	}
 }
